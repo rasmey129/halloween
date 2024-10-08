e10be6dee0bb7861f953aea5482565e3bcd2dce3
Rasmey Oung <158348034+rasmey129@users.noreply.github.com>
2024-10-08 18:28:23 -0400

    rewrote the widget to a basic one

--- lib/main.dart
+++ lib/main.dart
@@ -46,32 +46,20 @@ void initialState(){
   playBackground();
 }
 
+void dispose(){
+  backgroundPlayer.dispose();
+  super.dispose();
+}
 
   @override
-  Widget build(BuildContext context) {
-
+   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Halloween Interactive Experience'),
       ),
-      body: Center(
-        
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: const Center(
+        child: Text('Welcome to Halloween!'),
+      ),
     );
   }
 }
