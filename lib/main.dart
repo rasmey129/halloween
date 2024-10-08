7dd0dea2099774dc6a978a690a6eb67d7913deca
Rasmey Oung <158348034+rasmey129@users.noreply.github.com>
2024-10-08 18:40:51 -0400

    change most of the names and cleaned up code

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Halloween'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HalloweenScreen extends StatefulWidget {
+  const HalloweenScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HalloweenScreenState createState() => _HalloweenScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  
+class _HalloweenScreenState extends State<HalloweenScreen> {
   final AudioPlayer backgroundPlayer = AudioPlayer();
 
-Future<void> playBackground() async{
-  await backgroundPlayer.setAsset('assets');
-  backgroundPlayer.setLoopMode(LoopMode.one);
+  @override
+  void initState() {
+    super.initState();
+    playBackgroundMusic();
+  }
+
+  Future<void> playBackgroundMusic() async {
+    await backgroundPlayer.setAsset('assets');
+    backgroundPlayer.setLoopMode(LoopMode.one);
     backgroundPlayer.play();
-}
-
-void initialState(){
-  super.initState();
-  playBackground();
-}
-
-void dispose(){
-  backgroundPlayer.dispose();
-  super.dispose();
-}
+  }
 
   @override
-   Widget build(BuildContext context) {
+  void dispose() {
+    backgroundPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Halloween Interactive Experience'),
+        title: const Text('Halloween'),
       ),
       body: const Center(
         child: Text('Welcome to Halloween!'),
