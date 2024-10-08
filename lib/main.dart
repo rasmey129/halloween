a679db44020be55c7d2411d24df036943f2b76fc
Rasmey Oung <158348034+rasmey129@users.noreply.github.com>
2024-10-08 18:45:56 -0400

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'dart:math';
 
 class HalloweenScreen extends StatefulWidget {
   const HalloweenScreen({super.key});
@@ -38,6 +39,48 @@ class _HalloweenScreenState extends State<HalloweenScreen> {
       body: const Center(
         child: Text('Welcome to Halloween!'),
       ),
+    );
+  }
+}
+
+class SpookyCharacter extends StatefulWidget {
+  @override
+  SpookyCharacterState createState() => SpookyCharacterState();
+}
+
+class SpookyCharacterState extends State<SpookyCharacter>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  double topPosition = 0;
+  double leftPosition = 0;
+  final random = Random();
+   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat(reverse: true);
+    _controller.addListener(() {
+      setState(() {
+        topPosition = random.nextDouble() * 300;
+        leftPosition = random.nextDouble() * 300;
+      });
+    });
+   }
+
+   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: topPosition,
+      left: leftPosition,
+      child: Image.asset('assets/images/ghost.png', width: 50),
     );
   }
 }
